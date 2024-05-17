a864f0694fcac3f39b850b364e052706bcb35601
Taiwo Oladipupo <telleosis@gmail.com>
2024-05-17 15:52:54 +0100

    Update api_config.dart

--- lib/common/network/api_config.dart
+++ lib/common/network/api_config.dart
@@ -1,8 +1,7 @@
 class ApiConfig {
   ApiConfig._();
 
-  static const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImExMGExYWMxLTY0ZDItNDRlZC1hZTlkLWFkYTA0Y2Y4MDlkMiIsImVtYWlsIjoic29wcmlueWVAZ21haWwuY29tIiwicGhvbmUiOiIrMjEzMTIzNDU2NzgiLCJpYXQiOjE2OTk0Njg0MjMsImV4cCI6MTg3MjI2ODQyMywiaXNzIjoiY29tLnJoZW1hbWVuYSJ9.mClgop5cd_iHMwsgdwl4knUyyYMdOKlg4YLF1Rw7Wqg";
+  static const token = "";
   static const String baseUrl = "https://api.dev.rhemamena.com";
   static const Duration receiveTimeout = Duration(milliseconds: 15000);
   static const Duration connectionTimeout = Duration(milliseconds: 15000);
