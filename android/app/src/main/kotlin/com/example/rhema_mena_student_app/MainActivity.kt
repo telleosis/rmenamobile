5a69938dd642163ac6a6892f45af5d9d1149dc60
Taiwo Oladipupo <telleosis@gmail.com>
2024-05-17 15:33:38 +0100

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/rhema_mena_student_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rhema_mena_student_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/rhema_mena_student_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rhema_mena_student_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
